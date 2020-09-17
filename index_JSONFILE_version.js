f88c5357a81a0fe784b634bd302f5e65c7c25a76
Guinn <Dan.Guinn@fike.com>
2020-09-17 00:17:57 -0500

    first code commit

---
+++ index_JSONFILE_version.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const url = require('url');
+const port=3000;
+var fs = require('fs');
+http.createServer(function(req, res){
+
+  //Process Parameters:
+  const queryObj = url.parse(req.url,true).query;
+  var userLat=queryObj.lat;
+  var userLng=queryObj.lng;
+
+  //If parameters are not empty proceed:
+  if(userLat!==undefined && userLng!==undefined){
+
+    //Response will be in JSON:
+    res.writeHead(200, {'Content-Type':'application/json'});
+
+    //Get All Pharmacy locations (a larger data set would require a database):
+    let file='./json/pharmacies.json';
+    let jsonData='';
+    fs.readFile(file, 'utf8', function(error,data){
+        if(error){
+          console.error(error);
+          res.end(JSON.stringify(error));
+        }
+        //jsonData=data;
+        //console.log(data);
+        let dataArray=JSON.parse(data);
+
+        //Calculate the distance of each in the data set:
+        let distMap = dataArray.map( function(item){
+          //"Great Circle Distance Formula" in JavaScript (miles):
+          let distance= 3963.0 * Math.acos(
+           Math.sin(item.latitude/57.2958) * Math.sin(userLat/57.2958)
+           + Math.cos(item.latitude/57.2958) * Math.cos(userLat/57.2958)
+           * Math.cos(userLng/57.2958 -item.longitude/57.2958)
+          );
+          item.distance=distance;
+          return item;
+        });
+
+        //Sort the array by distance:
+        distMap.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
+
+        //Output the first element of the array:
+        res.end(JSON.stringify(distMap[0]));
+    });
+    console.log("finished");
+  }else{
+    res.end(JSON.stringify({"error":"Sorry, your request must include lat and lng parameters."}));
+  }
+
+
+
+}).listen(port);
+
+//console.log("Listening on http://localhost:"+port);
+console.log(`Listening on http://localhost:${port}`);
---
+++ index_MySQL_version.js
@@ -0,0 +1,74 @@
+const http = require("http");
+const url = require('url');
+const port=3000;
+const fs = require('fs');
+const mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1lex2admit",
+    database: "rxsavings"
+});
+
+con.connect(function(error){
+  if(error){
+    console.log("Unable to connect");
+  }else{
+    console.log("Connected to database");
+  }
+});
+
+http.createServer(function(req, res){
+
+  //Process Parameters:
+  const queryObj = url.parse(req.url,true).query;
+  var lat=queryObj.lat;
+  var lng=queryObj.lng;
+
+  //If parameters are not empty proceed:
+  if(lat!==undefined && lng!==undefined){
+
+/*
+      //An Approximate version in SQL (see notes.txt):
+      let sql=`
+          SELECT name, address, city, state, zip, latitude, longitude, SQRT(
+          POW(69.1 * (latitude - ${lat}), 2) +
+          POW(69.1 * (${lng} - longitude) * COS(latitude / 57.3), 2)) AS distance
+          FROM pharmacies HAVING distance < 100 ORDER BY distance LIMIT 1;
+      `;
+*/
+      //Great Circle Distance Formula in SQL:
+      let sql=`
+        SELECT name, address, city, state, zip, latitude, longitude,
+        ( 3959 * acos( cos( radians(${lat}) ) * cos( radians( latitude ) )
+        * cos( radians( longitude ) - radians(${lng}) )
+        + sin( radians(${lat}) ) * sin(radians(latitude)) ) ) AS distance
+        FROM pharmacies
+        HAVING distance < 100
+        ORDER BY distance
+        LIMIT 1;
+      `;
+
+      //Run query and output:
+      console.log("Running query:\n"+sql);
+      con.query(sql, function(error, result, fields){
+        if(error){
+          console.log("Unable to get results");
+        }else{
+          console.log("Sending stringified result:\n"+JSON.stringify(result));
+          res.end(JSON.stringify(result));
+        }
+      });
+
+  }else{
+    //Fail if parameters not defined:
+    res.end(JSON.stringify({"error":"Sorry, your request must include lat and lng parameters in decimal degrees."}));
+  }
+
+
+
+}).listen(port);
+
+console.log(`Listening on http://localhost:${port}
+Sample Test: http://localhost:3000/?lat=38.887844&lng=-94.851011
+`);
